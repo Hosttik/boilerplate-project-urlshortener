69f4f5066964c8ac3d73d460b98d32caf9ea876f
kirill <k.malkov@life-stream.tv>
2018-11-28 01:05:23 +0300

    url shortener

--- server.js
+++ server.js
@@ -3,15 +3,23 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const dns = require('dns');
+mongoose.connect(process.env.MONGO_URI);
+const Schema = mongoose.Schema;
+const shortenerSchema = new Schema({original_url : { type: String, required: true },short_url : {type: Number, required: true},typeName:{ type: String, required: true }});
+const ShortenerUrl = mongoose.model('ShortenerUrl', shortenerSchema);
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
 
 var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended: false}));
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
@@ -25,10 +33,32 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
+
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.post("/api/shorturl/new", (req, res) => {
+  const url = req.body.url;
+  if(validUrl.isUri(url)) {
+    ShortenerUrl.count().then(count=>{
+      const shortenerUrl = new ShortenerUrl({ original_url: url ,short_url:++count,typeName:'shortenerUrl' });
+      shortenerUrl.save().then(data => {
+        res.json({original_url:data.original_url,short_url:data.short_url});
+      });
+    });
+  } else {
+    res.json({error:"invalid URL"});
+  }
+});
+
+app.get('/api/shorturl/:reqshorturl',(req, res) => {
+  const shortUrlNumber=req.params.reqshorturl;
+  ShortenerUrl.findOne({ short_url: shortUrlNumber}).then(data=>{
+    res.writeHead(301,{Location: data.original_url});
+    res.end();
+  });
 });
 
 
